715b0cc260f8b255a16f07fa78a5268810347b65
jhenivic <168014351+jhenivic@users.noreply.github.com>
2025-06-09 11:35:35 -0300

    Add files via upload

---
+++ Agrinho.Jhenifer/script.js
@@ -0,0 +1,16 @@
+// Função para aumentar fonte
+document.getElementById("aumentar-fonte").addEventListener("click", function() {
+    document.body.classList.add("aumento-fonte");
+    document.body.classList.remove("diminuir-fonte");
+});
+
+// Função para diminuir fonte
+document.getElementById("diminuir-fonte").addEventListener("click", function() {
+    document.body.classList.remove("aumento-fonte");
+    document.body.classList.add("diminuir-fonte");
+});
+
+// Função para mudar contraste
+document.getElementById("mudar-contraste").addEventListener("click", function() {
+    document.body.classList.toggle("alto-contraste");
+});
